5388f7860fbed8753943bf96e78956aced8e8585
Moiz Ahmed <moizahmed1996@hotmail.com>
2022-10-16 18:41:44 -0400

    Add ability for user to enter location name to obtain weather information

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let search = document.querySelector(".search");
+let searchBtn = document.querySelector(".search-btn");
+
+let locationName = document.querySelector(".location-name");
 let temp = document.querySelector(".temp");
 let maxTemp = document.querySelector(".max-temp");
 let minTemp = document.querySelector(".min-temp");
@@ -5,32 +9,40 @@ let feelsLike = document.querySelector(".feels-like");
 let humidity = document.querySelector(".humidity");
 let pressure = document.querySelector(".pressure");
 
-async function getWeather() {
-  const response = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=mississauga&appid=5c5e73f9e818d5c73005dfc4073fea98&units=metric"
-  );
-  const data = await response.json();
-  return data;
+async function searchLocation() {
+  try {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        search.value +
+        "&appid=5c5e73f9e818d5c73005dfc4073fea98&units=metric",
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const displayWeatherData = async () => {
-  let weatherData = await getWeather();
-  console.log(weatherData);
-  temp.append(" " + weatherData.main.temp);
-  maxTemp.append(" " + weatherData.main.temp_max);
-  minTemp.append(" " + weatherData.main.temp_min);
+searchBtn.addEventListener("click", async (e) => {
+  locationName.innerHTML = "";
+  temp.innerHTML = "";
+  maxTemp.innerHTML = "";
+  minTemp.innerHTML = "";
+  feelsLike.innerHTML = "Feels Like:";
+  humidity.innerHTML = "Humidity:";
+  pressure.innerHTML = "Pressure:";
 
-  const feelsLikeData = document.createElement("span");
-  feelsLikeData.textContent = " " + weatherData.main.feels_like;
-  feelsLike.appendChild(feelsLikeData);
+  const weather = await searchLocation();
+  console.log(weather);
 
-  const humidityData = document.createElement("span");
-  humidityData.textContent = " " + weatherData.main.humidity;
-  humidity.appendChild(humidityData);
+  locationName.append(" " + weather.name);
+  temp.append(" " + Math.floor(weather.main.temp) + "℃");
+  maxTemp.append(" " + Math.floor(weather.main.temp_max) + "℃");
+  minTemp.append(" " + Math.floor(weather.main.temp_min) + "℃");
+  feelsLike.append(" " + weather.main.feels_like);
+  humidity.append(" " + weather.main.humidity);
+  pressure.append(" " + weather.main.pressure);
 
-  const pressureData = document.createElement("span");
-  pressureData.textContent = " " + weatherData.main.pressure;
-  pressure.appendChild(pressureData);
-};
-
-displayWeatherData();
+  search.value = "";
+});
